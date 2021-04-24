055d27c9dbb7797141bd1b37e07342043a844442
Robinlemon <lewis-gibson@hotmail.com>
2021-04-24 13:45:28 +0100

    Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -135,7 +135,7 @@ module.exports = {
                 'plugin:@typescript-eslint/recommended',
                 'plugin:@typescript-eslint/recommended-requiring-type-checking',
                 'plugin:import/typescript',
-                'prettier/@typescript-eslint',
+                'prettier',
             ],
             parser: '@typescript-eslint/parser',
             parserOptions: {
