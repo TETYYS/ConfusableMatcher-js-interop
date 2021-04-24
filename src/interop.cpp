0e166080d3c1ef015e082199514d0c75dd1b9217
Robinlemon <lewis-gibson@hotmail.com>
2021-04-24 23:02:39 +0100

    Remove validation from interop

--- src/interop.cpp
+++ src/interop.cpp
@@ -9,45 +9,34 @@ ConfusableMatcherNapiInterop::ConfusableMatcherNapiInterop(const Napi::CallbackI
      * Mappings
      */
     std::vector<std::pair<std::string, std::string>> mapVector;
-    if (info.Length() > 0 && info[0].IsArray())
+    Napi::Array mapArray = info[0].As<Napi::Array>();
+    for (uint32_t x = 0; x < mapArray.Length(); x++)
     {
-        Napi::Array mapArray = info[0].As<Napi::Array>();
-        for (uint32_t x = 0; x < mapArray.Length(); x++)
-        {
-            Napi::Array result = mapArray.Get(x).As<Napi::Array>();
-            std::pair<std::string, std::string> kvPair = {
-                result.Get((uint32_t)0).As<Napi::String>().Utf8Value(),
-                result.Get((uint32_t)1).As<Napi::String>().Utf8Value()};
-            mapVector.push_back(kvPair);
-        }
+        Napi::Array result = mapArray.Get(x).As<Napi::Array>();
+        std::pair<std::string, std::string> kvPair = {
+            result.Get((uint32_t)0).As<Napi::String>().Utf8Value(),
+            result.Get((uint32_t)1).As<Napi::String>().Utf8Value()};
+        mapVector.push_back(kvPair);
     }
 
     /**
      * Skips
      */
     std::unordered_set<std::string> skipsSet;
-    if (info.Length() > 1 && info[1].IsArray())
+    Napi::Array skipsArray = info[1].As<Napi::Array>();
+    for (uint32_t x = 0; x < skipsArray.Length(); x++)
     {
-        Napi::Array skipsArray = info[1].As<Napi::Array>();
-        for (uint32_t x = 0; x < skipsArray.Length(); x++)
-        {
-            skipsSet.insert(skipsArray.Get(x).As<Napi::String>().Utf8Value());
-        }
+        skipsSet.insert(skipsArray.Get(x).As<Napi::String>().Utf8Value());
     }
+
+    /**
+     *
+     */
+    bool addDefaults = info[2].As<Napi::Boolean>().ToBoolean();
 
     try
     {
-        /**
-         * Default Values
-         */
-        if (info.Length() > 2 && info[2].IsBoolean())
-        {
-            this->_instance = new ConfusableMatcher(mapVector, skipsSet, info[2].As<Napi::Boolean>().ToBoolean());
-        }
-        else
-        {
-            this->_instance = new ConfusableMatcher(mapVector, skipsSet);
-        }
+        this->_instance = new ConfusableMatcher(mapVector, skipsSet, addDefaults);
     }
     catch (const std::runtime_error &error)
     {
@@ -81,46 +70,21 @@ Napi::Value ConfusableMatcherNapiInterop::getKeyMappings(const Napi::CallbackInf
 
 Napi::Value ConfusableMatcherNapiInterop::indexOf(const Napi::CallbackInfo &info)
 {
-    CMOptions cmOpts = {
-        .MatchRepeating = true,
-        .StartIndex = 0,
-        .StartFromEnd = false,
-        .StatePushLimit = 10000,
-        .MatchOnWordBoundary = false};
-
-    if (info.Length() > 2 && info[2].IsObject())
-    {
-        Napi::Object optionsObject = info[2].As<Napi::Object>();
-
-        if (optionsObject.Has("matchRepeating"))
-        {
-            cmOpts.MatchRepeating = optionsObject.Get("matchRepeating").As<Napi::Boolean>().ToBoolean();
-        }
-        if (optionsObject.Has("startIndex"))
-        {
-            cmOpts.StartIndex = optionsObject.Get("startIndex").As<Napi::Number>().ToNumber().Uint32Value();
-        }
-
-        if (optionsObject.Has("startFromEnd"))
-        {
-            cmOpts.StartFromEnd = optionsObject.Get("startFromEnd").As<Napi::Boolean>().ToBoolean();
-        }
-        if (optionsObject.Has("statePushLimit"))
-        {
-            cmOpts.StatePushLimit = optionsObject.Get("statePushLimit").As<Napi::Number>().ToNumber().Uint32Value();
-        }
-        if (optionsObject.Has("matchOnWordBoundary"))
-        {
-            cmOpts.MatchOnWordBoundary = optionsObject.Get("matchOnWordBoundary").As<Napi::Boolean>().ToBoolean();
-        }
-    }
-
-    CMReturn result = this->_instance->IndexOf(
-        info[0].As<Napi::String>().Utf8Value(),
-        info[1].As<Napi::String>().Utf8Value(),
-        cmOpts);
-
     Napi::Env env = info.Env();
+
+    std::string in = info[0].As<Napi::String>().Utf8Value();
+    std::string needle = info[1].As<Napi::String>().Utf8Value();
+    Napi::Object optionsObject = info[2].As<Napi::Object>();
+
+    CMOptions cmOpts = {
+        .MatchRepeating = optionsObject.Get("matchRepeating").As<Napi::Boolean>().ToBoolean(),
+        .StartIndex = optionsObject.Get("startIndex").As<Napi::Number>().ToNumber().Uint32Value(),
+        .StartFromEnd = optionsObject.Get("startFromEnd").As<Napi::Boolean>().ToBoolean(),
+        .StatePushLimit = optionsObject.Get("statePushLimit").As<Napi::Number>().ToNumber().Uint32Value(),
+        .MatchOnWordBoundary = optionsObject.Get("matchOnWordBoundary").As<Napi::Boolean>().ToBoolean()};
+
+    CMReturn result = this->_instance->IndexOf(in, needle, cmOpts);
+
     Napi::Object obj = Napi::Object::New(env);
     obj.Set(Napi::String::New(env, "size"), Napi::Number::New(env, result.Size));
     obj.Set(Napi::String::New(env, "start"), Napi::Number::New(env, result.Start));
