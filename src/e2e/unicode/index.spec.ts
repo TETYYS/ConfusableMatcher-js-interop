e796e0bfe37adf3a7d8948132d3f0224f6d3aef1
Robinlemon <lewis-gibson@hotmail.com>
2021-04-24 17:05:08 +0100

    Remove old e2e

--- src/e2e/unicode/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConfusableMatcher } from '../../wrapper';
-
-describe('unicode', () => {
-    it('should match swastikas', () => {
-        const m = new ConfusableMatcher([['卐', '卐']], []);
-        expect(m.indexOf('leafy卐\n卐\n卐\n卐\n卐\n卐\n卐\n卐\n卐\n卐\n卐\n卐\n卐', '卐').index).toBeGreaterThan(-1);
-    });
-});
