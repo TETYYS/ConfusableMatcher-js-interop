38729f641741fad5e660fbd83b664ef99ee498e6
Robinlemon <lewis-gibson@hotmail.com>
2021-04-25 16:20:05 +0100

    Clean up api

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -10,18 +10,13 @@ const DEFAULT_OPTIONS: IIndexOfOptions = {
 };
 
 export class ConfusableMatcher {
-    private readonly _map: Mapping[] = [];
+    private readonly _maps: Mapping[];
     private readonly _skips: Set<string>;
 
     private _instance!: ConfusableMatcherInstance;
 
-    public constructor(map: Mapping[] = [], skips: Iterable<string> = [], addDefaultValues = true) {
-        if (map.length) {
-            for (const [key, value] of map) {
-                this._addMap(key, value);
-            }
-        }
-
+    public constructor(maps: Mapping[] = [], skips: Iterable<string> = [], addDefaultValues = true) {
+        this._maps = maps;
         this._skips = new Set(skips);
 
         if (addDefaultValues) {
@@ -45,22 +40,28 @@ export class ConfusableMatcher {
                 ' ',
                 '　',
             ];
-            for (const space of spaces) {
-                this._addMap(' ', space);
-            }
+            this._addMappings(
+                spaces.map((s) => [' ', s]),
+                false
+            );
 
             for (let c = 'A'.charCodeAt(0); c <= 'Z'.charCodeAt(0); c++) {
                 const lc = String.fromCharCode(c);
                 const uc = String.fromCharCode(c + 0x20);
-                this._addMap(lc, lc);
-                this._addMap(lc, uc);
-                this._addMap(uc, uc);
-                this._addMap(uc, lc);
+                this._addMappings(
+                    [
+                        [lc, lc],
+                        [lc, uc],
+                        [uc, uc],
+                        [uc, lc],
+                    ],
+                    false
+                );
             }
 
             for (let c = '0'.charCodeAt(0); c <= '9'.charCodeAt(0); c++) {
                 const char = String.fromCharCode(c);
-                this._addMap(char, char);
+                this._addMappings([[char, char]], false);
             }
         }
 
@@ -69,222 +70,134 @@ export class ConfusableMatcher {
 
     //#region Mappings
     public addMapping(key: string, value: string): void {
-        this._addMap(key, value);
-        this._rebuildInstance();
+        this._addMappings([[key, value]], true);
     }
 
     public addMappings(mappings: Iterable<Mapping>): void {
-        for (const [key, value] of mappings) {
-            this._addMap(key, value);
-        }
-
-        this._rebuildInstance();
+        this._addMappings(mappings, true);
     }
 
     public removeMapping(key: string, value: string): void {
-        if (key.length === 0) {
-            throw new Error('Key provided is empty.');
-        }
-        if (key[0] == '\x00' || key[0] == '\x01') {
-            throw new Error('Key cannot begin with \\x00 or \\x01');
-        }
+        this._removeMappings([[key, value]], true);
+    }
+
+    public removeMappings(mappings: Mapping[]): void {
+        this._removeMappings(mappings, true);
+    }
+
+    public getMappings(): Mapping[] {
+        return this._maps;
+    }
+
+    public getKeyMappings(value: string): string[] {
+        this._validateValue(value, 'Value');
+        return this._instance.getKeyMappings(value);
+    }
+    //#endregion Mappings
+
+    //#region Skips
+    public addSkip(skip: string): void {
+        this._addSkips([skip], true);
+    }
+
+    public addSkips(skips: string[]): void {
+        this._addSkips(skips, true);
+    }
+
+    public removeSkip(skip: string): void {
+        this._removeSkips([skip], true);
+    }
+
+    public removeSkips(skips: string[]): void {
+        this._removeSkips(skips, true);
+    }
+    //#endregion Skips
+
+    //#region Comparators
+    public indexOf(input: string, needle: string, options?: Partial<IIndexOfOptions>): IResult {
+        return this._instance.indexOf(input, needle, this._fillDefaultOptions(options));
+    }
+
+    public contains(input: string, needle: string, options?: IIndexOfOptions): boolean {
+        const result = this._instance.indexOf(input, needle, this._fillDefaultOptions(options));
+        return result.status === EReturnStatus.MATCH;
+    }
+    //#endregion Comparators
+
+    private _validateValue(value: string, name: string): void {
         if (value.length === 0) {
-            throw new Error('Value provided is empty.');
+            throw new Error(`${name} cannot be empty.`);
         }
         if (value[0] == '\x00' || value[0] == '\x01') {
-            throw new Error('Value cannot begin with \\x00 or \\x01');
+            throw new Error(String.raw`${name} cannot start with \x00 or \x01.`);
+        }
+    }
+
+    private _addMappings(mappings: Iterable<Mapping>, rebuild: boolean): void {
+        for (const [key, value] of mappings) {
+            this._validateValue(key, 'Key');
+            this._validateValue(value, 'Value');
+            this._maps.push([key, value]);
         }
 
-        let didRemove = false;
-        for (let i = 0; i < this._map.length; i++) {
-            const mapping = this._map[i];
-            if (mapping[0] !== key) {
-                continue;
-            }
-            if (mapping[1] !== value) {
-                continue;
-            }
-            this._map.splice(i, 1);
-            didRemove = true;
-            break;
-        }
-
-        if (didRemove) {
+        if (rebuild) {
             this._rebuildInstance();
         }
     }
 
-    /**
-     * @TODO
-     * O(m * n)
-     */
-    public removeMappings(mappings: Mapping[]): void {
-        if (mappings.length < 1) {
-            throw new Error('At least one mapping must be provided.');
-        }
-
+    private _removeMappings(mappings: Iterable<Mapping>, rebuild: boolean) {
         let didRemoveAny = false;
         for (const [key, value] of mappings) {
-            if (key.length === 0) {
-                throw new Error('Mapping key provided is empty.');
-            }
-            if (key[0] == '\x00' || key[0] == '\x01') {
-                throw new Error('Mapping key cannot begin with \\x00 or \\x01');
-            }
-            if (value.length === 0) {
-                throw new Error('Mapping value provided is empty.');
-            }
-            if (value[0] == '\x00' || value[0] == '\x01') {
-                throw new Error('Mapping value cannot begin with \\x00 or \\x01');
-            }
+            this._validateValue(key, 'Key');
+            this._validateValue(value, 'Value');
 
-            for (let i = 0; i < this._map.length; i++) {
-                const mapping = this._map[i];
+            for (let i = 0; i < this._maps.length; i++) {
+                const mapping = this._maps[i];
                 if (mapping[0] !== key) {
                     continue;
                 }
                 if (mapping[1] !== value) {
                     continue;
                 }
-                this._map.splice(i, 1);
+                this._maps.splice(i, 1);
                 didRemoveAny = true;
                 break;
             }
         }
 
-        if (didRemoveAny) {
+        if (didRemoveAny && rebuild) {
             this._rebuildInstance();
         }
     }
 
-    public getMappings(): Mapping[] {
-        return this._map;
-    }
-
-    public getKeyMappings(key: string): string[] {
-        if (key.length === 0) {
-            throw new Error('Key provided is empty.');
-        }
-        if (key[0] == '\x00' || key[0] == '\x01') {
-            throw new Error('Key cannot begin with \\x00 or \\x01');
-        }
-
-        return this._instance.getKeyMappings(key);
-    }
-    //#endregion Mappings
-
-    //#region Skips
-    public addSkip(skip: string): void {
-        if (skip.length === 0) {
-            throw new Error('Skip provided is empty.');
-        }
-        if (skip[0] == '\x00' || skip[0] == '\x01') {
-            throw new Error('Skip cannot begin with \\x00 or \\x01');
-        }
-
-        this._skips.add(skip);
-        this._rebuildInstance();
-    }
-
-    public addSkips(skips: string[]): void {
+    private _addSkips(skips: string[], rebuild: boolean): void {
         for (const skip of skips) {
-            if (skip.length === 0) {
-                throw new Error('Skip provided is empty.');
-            }
-            if (skip[0] == '\x00' || skip[0] == '\x01') {
-                throw new Error('Skip cannot begin with \\x00 or \\x01');
-            }
-
+            this._validateValue(skip, 'Skip');
             this._skips.add(skip);
         }
 
-        this._rebuildInstance();
-    }
-
-    public removeSkip(skip: string): void {
-        if (skip.length === 0) {
-            throw new Error('Skip provided is empty.');
-        }
-        if (skip[0] == '\x00' || skip[0] == '\x01') {
-            throw new Error('Skip cannot begin with \\x00 or \\x01');
-        }
-
-        const didRemove = this._skips.delete(skip);
-        if (didRemove) {
+        if (rebuild) {
             this._rebuildInstance();
         }
     }
 
-    /**
-     *
-     */
-    public removeSkips(skips: string[]): void {
-        if (skips.length < 1) {
-            throw new Error('At least one skip must be provided.');
-        }
-
+    private _removeSkips(skips: string[], rebuild: boolean): void {
         let didRemoveAny = false;
         for (const skip of skips) {
-            if (skip.length === 0) {
-                throw new Error('Skip provided is empty.');
-            }
-            if (skip[0] == '\x00' || skip[0] == '\x01') {
-                throw new Error('Skip cannot begin with \\x00 or \\x01');
-            }
-            const didRemove = this._skips.delete(skip);
-            if (didRemove && !didRemoveAny) {
+            this._validateValue(skip, 'Skip');
+
+            if (this._skips.delete(skip)) {
                 didRemoveAny = true;
             }
         }
 
-        if (didRemoveAny) {
+        if (didRemoveAny && rebuild) {
             this._rebuildInstance();
         }
     }
 
-    public indexOf(input: string, needle: string, options?: Partial<IIndexOfOptions>): IResult {
-        const result = this._instance.indexOf(input, needle, this._fillDefaultOptions(options));
-
-        switch (result.status) {
-            case EReturnStatus.MATCH:
-                return result;
-            case EReturnStatus.NO_MATCH:
-                return result;
-            // throw new Error();
-            case EReturnStatus.STATE_PUSH_LIMIT_EXCEEDED:
-                return result;
-            // throw new Error();
-            case EReturnStatus.WORD_BOUNDARY_FAIL_END:
-                return result;
-            case EReturnStatus.WORD_BOUNDARY_FAIL_START:
-                return result;
-            // throw new Error();
-        }
-    }
-
-    public contains(input: string, needle: string, options?: IIndexOfOptions): boolean {
-        return this._instance.indexOf(input, needle, this._fillDefaultOptions(options)).start > -1;
-    }
-
-    private _addMap(key: string, value: string) {
-        if (key.length === 0) {
-            throw new Error('Key provided is empty.');
-        }
-        if (key[0] == '\x00' || key[0] == '\x01') {
-            throw new Error('Key cannot begin with \\x00 or \\x01');
-        }
-        if (value.length === 0) {
-            throw new Error('Value provided is empty.');
-        }
-        if (value[0] == '\x00' || value[0] == '\x01') {
-            throw new Error('Value cannot begin with \\x00 or \\x01');
-        }
-
-        this._map.push([key, value]);
-    }
-
     private _rebuildInstance(): void {
-        this._instance = new ConfusableMatcherInterop([...this._map.values()], [...this._skips.values()], false);
+        this._instance = new ConfusableMatcherInterop([...this._maps.values()], [...this._skips.values()], false);
     }
 
     private _fillDefaultOptions(options?: Partial<IIndexOfOptions>): IIndexOfOptions {
