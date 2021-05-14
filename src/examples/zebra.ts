ebf27dbcf2bd6933c29e9b318e5d21f5b57d7f58
Robinlemon <lewis-gibson@hotmail.com>
2021-05-14 20:24:19 +0100

    Add str pos pointer to zebra example

--- src/examples/zebra.ts
+++ src/examples/zebra.ts
@@ -4,16 +4,26 @@ import { ConfusableMatcher, EReturnStatus } from '..';
 const map: Mapping[] = [['Z', 'Ž']];
 const skips: string[] = [' ', '_', '-'];
 const cm = new ConfusableMatcher(map, skips, true);
+
+const input =
+    'Žebras are a short, stocky animal that is generally about 8 feet long and stands between 4 and 5 feet at the shoulder.';
+const needle = 'Zebras';
+
+let strPosPtrs: number | undefined = cm.computeStringPosPointers(needle);
+
 const options: Partial<IIndexOfOptions> = {
     matchOnWordBoundary: true,
     matchRepeating: true,
+    needlePosPointers: strPosPtrs,
     startFromEnd: false,
     startIndex: 0,
     statePushLimit: 10_000,
 };
-const input =
-    'Žebras are a short, stocky animal that is generally about 8 feet long and stands between 4 and 5 feet at the shoulder.';
-const needle = 'Zebras';
 const result: IResult = cm.indexOfSync(input, needle, options);
+
+cm.freeStringPosPointers(strPosPtrs);
+strPosPtrs = undefined;
+delete options.needlePosPointers;
+
 const status: EReturnStatus = result.status;
 console.table({ ...result, status: EReturnStatus[status] });
