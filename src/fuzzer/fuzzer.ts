d3b0ffd66f25de84b715a2c69e3a92694cc6e14b
Robinlemon <lewis-gibson@hotmail.com>
2021-06-26 13:58:00 +0100

    Support fuzzing skips too

--- src/fuzzer/fuzzer.ts
+++ src/fuzzer/fuzzer.ts
@@ -3,27 +3,52 @@ import { ConfusableMatcher } from '../wrapper';
  * Maps should be an array of objects with key/value fields.
  */
 import Maps from './resources/maps.json';
+/**
+ * Maps should be an array of objects with a value field.
+ */
+import Skips from './resources/skips.json';
 
 console.log(`Loaded ${Maps.length} Maps`);
+console.log(`Loaded ${Skips.length} Skips`);
 
 process.once('SIGSEGV', (signal) => {
     console.error(signal);
 });
 
-console.log('Starting');
-
-const SKIP_INDEXEX = new Set<number>([
+/**
+ * Maps
+ */
+const MAP_INDEX_SKIPS = new Set<number>([
     /* Write indexes to skip */
 ]);
-
+console.log('Testing Maps');
 for (let i = 0; i < Maps.length; i++) {
     const { key, value } = Maps[i];
-    if (SKIP_INDEXEX.has(i)) {
+    if (MAP_INDEX_SKIPS.has(i)) {
         console.log(`Skipping ${i}: '${key}' -> '${value}'`);
         continue;
     }
 
-    console.log(`Trying ${i}: '${key}' -> '${value}'`);
+    console.log(`Trying Map ${i}: '${key}' -> '${value}'`);
     new ConfusableMatcher([[key, value]]);
 }
-console.log('Success!');
+console.log('Maps Success!');
+
+/**
+ * Maps
+ */
+const SKIP_INDEX_SKIPS = new Set<number>([
+    /* Write indexes to skip */
+]);
+console.log('Testing Skips');
+for (let i = 0; i < Skips.length; i++) {
+    const { value } = Skips[i];
+    if (MAP_INDEX_SKIPS.has(i)) {
+        console.log(`Skipping ${i}: '${value}'`);
+        continue;
+    }
+
+    console.log(`Trying Skip ${i}: '${value}'`);
+    new ConfusableMatcher([], [value]);
+}
+console.log('Skips Success!');
